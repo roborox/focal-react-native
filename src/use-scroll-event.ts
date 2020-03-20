83d149f4c176696c0ebed6f35440d3ee38498d87
vwpo <vwpo@roborox.org>
2020-03-20 13:46:05 +0300

    feat(*): now useScrollEvent accepts Observable

--- src/use-scroll-event.ts
+++ src/use-scroll-event.ts
@@ -1,17 +1,22 @@
 import { NativeSyntheticEvent, NativeScrollEvent } from "react-native"
 import { InfiniteListState } from "@roborox/focal-react/build/src/infinite-list/domain"
+import { Observable } from "rxjs"
+import { useRx } from "@roborox/focal-react/build/src/use-rx"
 
 export const isScrollCloseToBottom = (offset: number, ev: NativeSyntheticEvent<NativeScrollEvent>) => {
 	return ev.nativeEvent.layoutMeasurement.height + ev.nativeEvent.contentOffset.y >=
 	ev.nativeEvent.contentSize.height - offset
 }
 
-export const useScrollEvent = (state: InfiniteListState<any, any>, offset = 50) => {
+export const useScrollEvent = (rxState: Observable<InfiniteListState<any, any>>, offset = 50) => {
+	const state = useRx(rxState)
+
 	return (ev: NativeSyntheticEvent<NativeScrollEvent>, loadNext?: () => Promise<void>) => {
-		if (loadNext
+		if ( state
+			&& loadNext
 			&& isScrollCloseToBottom(offset, ev)
-			&& !state.finished
 			&& state.status.status === "success"
+			&& !state.finished
 		) {
 			loadNext()
 		}
